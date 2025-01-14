65ba787f8379dae6145662a17179451d74fc5f14
Eros Mendonça <erosmendonca@outlook.com>
2025-01-14 19:58:26 +0000

    feat: introducao

---
+++ lessons/001-variables/01-intro.js
@@ -0,0 +1,30 @@
+// ================ O que é uma variável? ===============
+console.log("================ O que é uma variável? ===============")
+// Uma variável é um espaço na memória onde armazenamos dados, como números, texto, etc
+// Estamos armazenando um nome
+let myName = "Adriano Nocelo";
+
+// Aqui estamos printando o nome
+console.log(myName);
+console.log("\n");
+
+// =============== Tipos de Variáveis ===============
+console.log("=============== Tipos de Variáveis ===============")
+// Alguns Tipos Disponíveis em Linguagens de Programação:
+// String - Texto - Ex: "Meu Nome" ou 'Meu Nome'
+// Int - Números Inteiros - Ex: 0, 256, -20, 25
+// Float - Números Flutuantes - Ex: 2.5, 35.555, -123.567
+// Bool - Verdadeiro ou Falso - true/false
+// Null - Valor nulo - null
+
+let myName2 = "Adriano Nocelo";
+let idade = 24;
+let altura = 1.80;
+let gostaDeProgramacao = true;
+let valorNull = null;
+
+console.log(myName2);
+console.log(idade);
+console.log(altura);
+console.log(gostaDeProgramacao);
+console.log(valorNull);
