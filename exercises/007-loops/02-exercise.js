f1774753f77fa4f94c78814a1119f643ae055dc6
Eros Mendonça <erosmendonca@outlook.com>
2025-03-18 19:54:52 +0000

    exercicio 2 loops

--- exercises/007-loops/02-exercise.js
+++ exercises/007-loops/02-exercise.js
@@ -5,11 +5,29 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let numero = await rl.question("Digite um número: ");
+const numero = Number(await rl.question("Digite um número: "));
+let acc = 0;
+let strAcc = "";
 
+// inicializacao, condicao, incremento
 for (let i = 1; i <= numero; i++) {
-    const soma = Number(i) + Number(numero) ;
-    console.log(numero + " + " + i + " = " + soma)
+  console.log("======= Iteracao " + i + "=======")
+  console.log("acc: " + acc)
+  console.log("i: " + i)
+
+  acc = acc + i;
+
+  // Verifico se é a última volta e coloco o simbolo de igual ao inves do mais
+  if (i === numero) {
+    strAcc = strAcc + i + " = "
+  } else {
+    strAcc = strAcc + i + " + "
+  }
+
+  console.log("strAcc: " + strAcc)
+  console.log("===== Fim Iteracao " + i + "=====")
 }
+
+console.log(strAcc + acc);
 
 rl.close();
